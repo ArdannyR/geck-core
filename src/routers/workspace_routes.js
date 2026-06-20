d2014922e503bb4c19ee67a964edfa5c7ae80c4c
Arda999-prog <ardannyromero@gmail.com>
2026-06-20 17:19:46 -0500

    arreglo en rutas de workspace

--- src/routers/workspace_routes.js
+++ src/routers/workspace_routes.js
@@ -2,9 +2,10 @@ import { Router } from 'express';
 import { createWorkspace, inviteMember, fetchUserWorkspaces, acceptInvite, leaveWorkspace } from '../controllers/workspace_controller.js';
 import { verifyAuth } from '../middlewares/auth.js';
 
+const router = Router();
+
 router.get('/accept-invite/:token', acceptInvite);
 
-const router = Router();
 router.use(verifyAuth);
 
 router.post('/create', createWorkspace);
